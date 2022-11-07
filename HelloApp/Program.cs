c3eac0853c8128f2bf6431a1742015e98b81d10f
black <black@example.com>
2022-11-08 00:21:56 +0500

    hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("hello");
 Console.WriteLine("hello white");
+=======
+Console.WriteLine("hello everyone");
+>>>>>>> 9979720 (hello everyone)
